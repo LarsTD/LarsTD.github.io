dbadc8406467eb37c77feb2a6a1176b9ff516cc1
LarsTD <larsb1314@gmail.com>
2018-12-26 15:49:59 +0100

    updated instructions

---
+++ ex07.py
@@ -0,0 +1,13 @@
+answer = ''
+
+while not answer:
+    print('do you prefer apples or bananas?')
+    answer = input()
+    if answer == 'apples':
+        print('you prefer apples!')
+    elif answer == 'bananas':
+        print('you prefer bananas')
+    else:
+        print('that is not a valid answer! answer again!')
+
+
